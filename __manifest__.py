23ff0874e6f0f511e212701efbd5056f90467741
usbpendrive <ferry.fernando@gmail.com>
2018-12-22 07:51:23 +0700

    Create new odoo module directory structure.

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo My Library Module',
+    'summary': 'Manage Books Easily',
+    'description': """
+    Manage your books library easily using this module.
+    """,
+    'author': 'Fernando',
+    'license': 'LGPL-3',
+    'application': True,
+    'version': '12.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+    ]
+}
